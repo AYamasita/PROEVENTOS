2777979099020694a0969974926d7c6d8b9f8adb
ayamasita <alexandreyamasita@gmail.com>
2022-01-26 17:04:29 -0300

    Seção 5: Angular, Interpolação, Diretiva e Binding

--- Front/ProEventos-App/src/app/app.module.ts
+++ Front/ProEventos-App/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
@@ -6,19 +7,25 @@ import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component';
+
+import { CollapseModule } from 'ngx-bootstrap/collapse';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     EventosComponent,
-    PalestrantesComponent
+    PalestrantesComponent,
+    NavComponent
    ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CollapseModule.forRoot(),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent] //inicializa
--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-eventos',
@@ -8,38 +9,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public eventos: any = [];
+  public eventosFiltrados: any = [];
+
+  imagemAltura: number =100;
+  imagemMargem: number =2;
+  mostrarImagem: boolean = true;
+  private _filtroLista : string = "";
 
   constructor(private http: HttpClient) { }
 
-
   ngOnInit(): void {
     this.getEventos();
+  }
+
+  public get filtroLista(): string
+  {
+    return this._filtroLista;
+  }
+  public set filtroLista(value : string)
+  {
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
 
   }
 
+public filtrarEventos(filtrarPor : string): any
+{
+  filtrarPor = filtrarPor.toLocaleLowerCase();
+  return this.eventos.filter(
+    (evento: { tema: string; local: string; })=>
+        evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+        evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    );
+}
+
+
   public getEventos(): void
   {
+    //it is recommended only to use the anonymous function if you only specify the next callback otherwise we recommend to pass an Observer
+    this.http.get('https://localhost:5001/api/eventos').subscribe({
+      next: (response) => {
+            this.eventos = response;
+            this.eventosFiltrados = this.eventos;
+      },
+      error: (error) => console.error(error)}
 
-    this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response,
-      error => console.log(error)
-    );
+    )
 
-    //  this.eventos = [
-    //   {
-    //     Tema: 'Angular 11',
-    //     Local: 'Belo Horizonte'
-    //   },
-    //   {
-    //     Tema: 'Dot Net 5',
-    //     Local: 'São Paulo'
-    //   },
-    //   {
-    //     Tema: 'Angular e suas novidades',
-    //     Local: 'Rio de Janeiro'
-    //   }
-    // ]
+    //https://rxjs.dev/deprecations/subscribe-arguments
+    // this.http.get('https://localhost:5001/api/eventos').subscribe(
+    //   response => this.eventos = response,
+    //   error => console.log(error)
+    // );
+  }
+
+  public visualiarImagem()
+  {
+    this.mostrarImagem = !this.mostrarImagem;
   }
 
 }
